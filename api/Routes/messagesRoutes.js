e910bb7732af1dfe3b2ae89eafd6b355a56a0e6e
RimshaRiaz-22 <rimshanimo22@gmail.com>
2022-12-16 11:32:08 +0500

    filters for chat

--- api/Routes/messagesRoutes.js
+++ api/Routes/messagesRoutes.js
@@ -8,5 +8,9 @@ router.post("/getStaredMsgsByuserId", controller.getStaredMessagesByUserId)
 router.post("/UpdateReadDeliveredTimeMessages", controller.ReadTimeAndDeliveredMessages)
 router.post("/get-single-msg", controller.getSingleMessage)
 router.post("/delete-bulk-of-msgs", controller.DeletBulkOfMsgs)
+router.post("/get-all-msgs-by-userId", controller.GetAllMsgsByUserId)
+router.post("/filter-all-chat-of-user", controller.FilterAllChatByUserId)
+router.post("/filter-single-chatlist-of-user", controller.FilterSingleChatByUserId)
+router.post("/filter-stared-chatlist-of-user", controller.FilterStaredChatByUserId)
 
 module.exports = router;
